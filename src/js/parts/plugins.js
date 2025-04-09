338f0a8da7d00506a749b488e0af5b6471e3b8ef
niralikarmadhi <118166254+niralikarmadhi@users.noreply.github.com>
2025-04-09 17:10:12 +0530

    second section

--- src/js/parts/plugins.js
+++ src/js/parts/plugins.js
@@ -1,9 +1,14 @@
 import "slick-carousel";
 
 export class Plugins {
-
   init() {
-
+    this.flavourSlider();
   }
-
+  flavourSlider() {
+    $(".flavour-slider").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots:false,
+    });
+  }
 }
