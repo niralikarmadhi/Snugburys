27f473a3f116464db40e8a666bbda7d418b368f3
varsha <varsha@karmadhi.com>
2025-04-09 17:10:08 +0530

    home page complete

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20450,7 +20450,58 @@ var Parts = /*#__PURE__*/function () {
   }
   parts_createClass(Parts, [{
     key: "init",
-    value: function init() {}
+    value: function init() {
+      this.LatestSlider();
+      this.CenterImgSlider();
+    }
+  }, {
+    key: "LatestSlider",
+    value: function LatestSlider() {
+      $('.latest-news-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: false,
+        arrows: false,
+        responsive: [{
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            slidesToShow: 3
+          }
+        }, {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            slidesToShow: 1
+          }
+        }]
+      });
+    }
+  }, {
+    key: "CenterImgSlider",
+    value: function CenterImgSlider() {
+      $('.center-img-content-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        arrows: true,
+        prevArrow: '.center-img-content-section .prev-arrow',
+        nextArrow: '.center-img-content-section .next-arrow',
+        responsive: [{
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            slidesToShow: 3
+          }
+        }, {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            slidesToShow: 1
+          }
+        }]
+      });
+    }
   }]);
   return Parts;
 }();
