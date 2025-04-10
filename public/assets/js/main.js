7bf5e5fdabbb99ddcae1b75d15b7a3b7d5c2f82e
varsha <varsha@karmadhi.com>
2025-04-10 15:36:56 +0530

    sculptures page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20505,6 +20505,8 @@ var Parts = /*#__PURE__*/function () {
     value: function init() {
       this.LatestSlider();
       this.CenterImgSlider();
+      this.RightImgSlider();
+      this.CreationSlider();
     }
   }, {
     key: "LatestSlider",
@@ -20539,6 +20541,58 @@ var Parts = /*#__PURE__*/function () {
         arrows: true,
         prevArrow: '.center-img-content-section .prev-arrow',
         nextArrow: '.center-img-content-section .next-arrow',
+        responsive: [{
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            slidesToShow: 3
+          }
+        }, {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            slidesToShow: 1
+          }
+        }]
+      });
+    }
+  }, {
+    key: "RightImgSlider",
+    value: function RightImgSlider() {
+      $('.right-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        arrows: true,
+        dots: true,
+        prevArrow: '.right-slider-section .prev-arrow',
+        nextArrow: '.right-slider-section .next-arrow',
+        responsive: [{
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            slidesToShow: 3
+          }
+        }, {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            slidesToShow: 1
+          }
+        }]
+      });
+    }
+  }, {
+    key: "CreationSlider",
+    value: function CreationSlider() {
+      $('.creations-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: false,
+        arrows: true,
+        dots: true,
+        prevArrow: '.creation-section .prev-arrow',
+        nextArrow: '.creation-section .next-arrow',
         responsive: [{
           breakpoint: 768,
           settings: {
